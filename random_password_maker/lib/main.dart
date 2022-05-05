1223aef86ca57e2a40e0b847fd048fe66ebf646b
nevarwin <86557872+nevarwin@users.noreply.github.com>
2022-05-05 21:16:09 +0800

    remove title

--- random_password_maker/lib/main.dart
+++ random_password_maker/lib/main.dart
@@ -17,15 +17,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Random Password Generator'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -71,7 +69,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text('Flutter Random Password Generator'),
       ),
       body: Center(
         child: Column(
